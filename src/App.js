f4161171e2be1008f0f2ba0b131b6e10182084e5
bschue <bschuess3@gmail.com>
2018-08-08 10:12:13 -0400

    formatted code

--- src/App.js
+++ src/App.js
@@ -4,31 +4,38 @@ import './App.css';
 const githubLink = "https://api.github.com/users/Bschues"
 
 class App extends Component {
-  constructor(props){
+
+  constructor(props)
+  {
     super(props)
     this.state = {
       user: {},
       active: false
     }
   }
-  //fetch data from github api link, and set the state to active or 
-  handleClick = () => {
+  //fetch data from github api link, 
+  //and set the state to active or inactive 
+  handleClick = () => 
+  {
     fetch(githubLink)
     .then((response) => response.json())
-    .then((data) => {this.setState(prevState => ({user: data, active: !prevState.active}))
-    })
+    .then((data) => {this.setState(prevState => ({user: data, active: !prevState.active}))})
   }
+
   render() {
+    const {user, active} = this.state
     return (
       <div id="mainDiv">
       <button onClick={this.handleClick}>Toggle User</button>
-      {this.state.active === true &&
+      {active === true &&
         <div id="subDiv">
-          <div><img src = {this.state.user.avatar_url} alt='Brian'></img></div>
+          <div id="imgDiv">
+            <img src = {user.avatar_url} alt='Brian'></img>
+          </div>
           <div id="infoDiv">
-          <div className='Info'>{this.state.user.name}</div>
-          <div id="bio" className='Info'>{this.state.user.bio}</div>
-          <div id="location" className='Info'>{this.state.user.location}</div>
+          <div className='Info'>{user.name}</div>
+          <div id="bio" className='Info'>{user.bio}</div>
+          <div id="location" className='Info'>{user.location}</div>
           </div>
         </div>
       }
